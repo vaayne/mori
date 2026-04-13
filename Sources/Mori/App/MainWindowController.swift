f5955101e3bb8a66bd1aca56a3d4921ac188d09b
Vaayne <liu.vaayne@gmail.com>
2026-04-13 10:59:44 +0800

    ✨ feat: add toolbar shortcuts for companion tools

--- Sources/Mori/App/MainWindowController.swift
+++ Sources/Mori/App/MainWindowController.swift
@@ -10,9 +10,13 @@ final class MainWindowController: NSWindowController {
     private enum ToolbarID {
         static let main = NSToolbar.Identifier("MoriMainToolbar")
         static let toggleSidebar = NSToolbarItem.Identifier("toggleSidebar")
+        static let files = NSToolbarItem.Identifier("openFiles")
+        static let git = NSToolbarItem.Identifier("openGit")
     }
 
     var onToggleSidebar: (() -> Void)?
+    var onToggleFiles: (() -> Void)?
+    var onToggleGit: (() -> Void)?
     var onShowCreateWorktreePanel: (() -> Void)?
 
     /// The hosting view for the update pill, overlaid on the titlebar.
@@ -110,6 +114,14 @@ final class MainWindowController: NSWindowController {
     @objc private func toggleSidebarClicked() {
         onToggleSidebar?()
     }
+
+    @objc private func toggleFilesClicked() {
+        onToggleFiles?()
+    }
+
+    @objc private func toggleGitClicked() {
+        onToggleGit?()
+    }
 }
 
 // MARK: - NSToolbarDelegate
@@ -135,11 +147,11 @@ extension MainWindowController: NSWindowDelegate {
 extension MainWindowController: NSToolbarDelegate {
 
     func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [ToolbarID.toggleSidebar, .flexibleSpace]
+        [ToolbarID.toggleSidebar, .flexibleSpace, ToolbarID.files, ToolbarID.git]
     }
 
     func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [ToolbarID.toggleSidebar, .flexibleSpace]
+        [ToolbarID.toggleSidebar, ToolbarID.files, ToolbarID.git, .flexibleSpace]
     }
 
     func toolbar(
@@ -147,14 +159,35 @@ extension MainWindowController: NSToolbarDelegate {
         itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier,
         willBeInsertedIntoToolbar flag: Bool
     ) -> NSToolbarItem? {
-        guard itemIdentifier == ToolbarID.toggleSidebar else { return nil }
-        let item = NSToolbarItem(itemIdentifier: ToolbarID.toggleSidebar)
-        item.label = .localized("Toggle Sidebar")
-        item.paletteLabel = .localized("Toggle Sidebar")
-        item.toolTip = .localized("Show or hide the sidebar (⌘0)")
-        item.image = NSImage(systemSymbolName: "sidebar.left", accessibilityDescription: .localized("Toggle Sidebar"))
-        item.target = self
-        item.action = #selector(toggleSidebarClicked)
-        return item
+        let item = NSToolbarItem(itemIdentifier: itemIdentifier)
+
+        switch itemIdentifier {
+        case ToolbarID.toggleSidebar:
+            item.label = .localized("Toggle Sidebar")
+            item.paletteLabel = .localized("Toggle Sidebar")
+            item.toolTip = .localized("Show or hide the sidebar (⌘0)")
+            item.image = NSImage(systemSymbolName: "sidebar.left", accessibilityDescription: .localized("Toggle Sidebar"))
+            item.target = self
+            item.action = #selector(toggleSidebarClicked)
+            return item
+        case ToolbarID.files:
+            item.label = .localized("Files")
+            item.paletteLabel = .localized("Files")
+            item.toolTip = .localized("Open Files Companion Pane (⌘E)")
+            item.image = NSImage(systemSymbolName: "folder", accessibilityDescription: .localized("Files"))
+            item.target = self
+            item.action = #selector(toggleFilesClicked)
+            return item
+        case ToolbarID.git:
+            item.label = .localized("Git")
+            item.paletteLabel = .localized("Git")
+            item.toolTip = .localized("Open Git Companion Pane (⌘G)")
+            item.image = NSImage(systemSymbolName: "point.topleft.down.curvedto.point.bottomright.up", accessibilityDescription: .localized("Git"))
+            item.target = self
+            item.action = #selector(toggleGitClicked)
+            return item
+        default:
+            return nil
+        }
     }
 }
